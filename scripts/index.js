a9834c40524ac495c42544d830005775b5f192d2
4iar <4iar@users.noreply.github.com>
2016-07-09 22:01:38 +0100

    Reorganise WxH variables

--- scripts/index.js
+++ scripts/index.js
@@ -14,22 +14,22 @@ function cleanup(data) {
 function render(data) {
   var data = cleanup(data);
 
-  var width = 900;
-  var height = 700;
   var margin = {
     left: 50,
     right: 50,
     top: 50,
     bottom: 50
   };
+  var width = 900 - margin.left - margin.right;
+  var height = 700 - margin.bottom - margin.top;
 
   var xScale = d3.scaleTime()
     .domain(d3.extent(data, function(d) { return d.date }))
-    .range([margin.left, width - margin.right]);
+    .range([margin.left, width]);
   
   var yScale = d3.scaleLinear()
     .domain([0, d3.max(data, function(d) { return d.gdp })])
-    .range([height - margin.top, margin.bottom]);
+    .range([height, margin.bottom]);
   
   var xAxis = d3.axisBottom()
     .scale(xScale)
@@ -39,16 +39,21 @@ function render(data) {
 
   var svg =  d3.select("body")
     .append("svg")
-    .attr("width", width)
-    .attr("height", height)
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
 
   svg.append('g')            
     .attr('class', 'x axis') 
-    .attr("transform", "translate(0," + (height - margin.bottom) + ")")
+    .attr("transform", "translate(0," + (height) + ")")
     .call(xAxis);           
 
   svg.append('g')          
     .attr('class', 'y axis')
     .attr("transform", "translate(" + (margin.left) + ",0)")
     .call(yAxis);          
+
+  
+  
+  
+  
 }
